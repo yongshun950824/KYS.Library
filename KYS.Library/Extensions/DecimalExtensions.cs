198ad93189c58e541e12a015a07c08b7a5a4cb5b
yongshun950824 <kokys-wa13@student.tarc.edu.my>
2022-08-21 21:28:58 +0800

    Extensions: - Add DecimalExtensions

---
+++ KYS.Library/Extensions/DecimalExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace KYS.Library.Extensions
+{
+    public static class DecimalExtensions
+    {
+        public static decimal Round(this decimal input, int places)
+        {
+            return Math.Round(input, places);
+
+            #region Alternate
+            //double num = Math.Pow(10, places);
+            //return Math.Ceiling(input * Convert.ToDecimal(num)) / Convert.ToDecimal(num);
+            #endregion
+        }
+    }
+}
