524472933b614bc982d666192de029a8d7f55cbb
yongshun950824 <kokys-wa13@student.tarc.edu.my>
2022-09-01 14:26:45 +0800

    SQLHelper: - Add BuildStructuredSqlParameter

--- KYS.Library/Helpers/SQLHelper.cs
+++ KYS.Library/Helpers/SQLHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Data;
+﻿using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace KYS.Library.Helpers
@@ -35,6 +36,44 @@ namespace KYS.Library.Helpers
             da.Fill(dt);
 
             return dt;
+        }
+
+        /// <summary>
+        /// Build structured parameter. Scenario: Query with <c>IN (SELECT [col] FROM @Parameter)</c>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="tableTypeName">Name of Table Type created in database.</param>
+        /// <param name="tableTypeColumnName">Column name in Table Type.</param>
+        /// <param name="parameterName">Parameter name.</param>
+        /// <param name="values">One or more values as the paramter value(s).</param>
+        /// <returns></returns>
+        /// <remarks>
+        /// <b>Pre-requisites</b> <br />
+        /// 1. Create a Table Type in database. <br />
+        /// SQL: <c>CREATE TYPE dbo.IdList AS TABLE ([Id] int PRIMARY KEY)</c> <br />
+        /// 2. SqlCommand: <br />
+        /// <c>SELECT [col] FROM [table] WHERE [col] IN (SELECT [tableTypeColumnName] FROM @Parameter)</c>
+        /// <br /><br />
+        /// <b>References</b>  <br />
+        /// 1. <a href="https://dotnetfiddle.net/jOePEn">Demo</a> <br />
+        /// 2. <a href="https://stackoverflow.com/questions/73561484/ado-net-query-returning-nothing-even-if-the-item-available">
+        ///     StackOverflow: ADO.NET - Query with <c>IN ()</c>
+        ///    </a>
+        /// </remarks>
+        public static SqlParameter BuildStructuredSqlParameter<T>(string tableTypeName, string tableTypeColumnName, 
+            string parameterName, IEnumerable<T> values)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add(tableTypeColumnName, typeof(T));
+
+            foreach (var @value in values)
+                dt.Rows.Add(@value);
+
+            return new SqlParameter(parameterName, SqlDbType.Structured)
+            {
+                Value = dt,
+                TypeName = tableTypeName,
+            };
         }
     }
 }
