59f7b9b482879b7bfb1efb95e33944a7e01c6573
yongshun950824 <yongshun950824@gmail.com>
2023-03-23 12:48:53 +0800

    - Add DateTime extension methods

---
+++ KYS.Library/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace KYS.Library.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// Reference: <a href="https://stackoverflow.com/q/114983/8017690">Given a DateTime object, how do I get an ISO 8601 date in string format?</a>
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <param name="cultureInfo"></param>
+        /// <returns></returns>
+        public static string ToIso8601String(this DateTime dt, CultureInfo cultureInfo = null)
+        {
+            return dt.ToString("O", cultureInfo ?? CultureInfo.InvariantCulture);
+        }
+
+        public static DateTime GetFirstDateOfMonth(this DateTime dt)
+        {
+            return new DateTime(dt.Year, dt.Month, 1);
+        }
+
+        public static DateTime GetLastDateOfMonth(this DateTime dt)
+        {
+            return dt.AddMonths(1).AddDays(-1);
+        }
+
+        public static DateTime GetFirstDateOfYear(this DateTime dt)
+        {
+            return new DateTime(dt.Year, 1, 1);
+        }
+
+        public static DateTime GetLastDateOfYear(this DateTime dt)
+        {
+            return new DateTime(dt.Year, 1, 1)
+                .AddYears(1)
+                .AddDays(-1);
+        }
+    }
+}
