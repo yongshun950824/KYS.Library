67ed349d9cc41c192b2dbe93e98cda1dfecdfb21
yongshun950824 <yongshun950824@gmail.com>
2023-07-05 09:08:04 +0800

    - Add SharpZipLib - FileHelper: Add ReadBase64StringToByteArray - Add ZipHelper helper

--- KYS.Library/Helpers/FileHelper.cs
+++ KYS.Library/Helpers/FileHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace KYS.Library.Helpers
 {
@@ -7,7 +8,7 @@ namespace KYS.Library.Helpers
         public static MemoryStream ToMemoryStream(byte[] byteArray)
         {
             MemoryStream ms = new MemoryStream();
-            ms.Write(byteArray);
+            ms.Write(byteArray, 0, byteArray.Length);
             ms.Position = 0;
 
             return ms;
@@ -16,6 +17,13 @@ namespace KYS.Library.Helpers
         public static byte[] ToByteArray(MemoryStream ms)
         {
             return ms.ToArray();
+        }
+
+        public static byte[] ReadBase64StringToByteArray(string base64String)
+        {
+            byte[] bytes = Convert.FromBase64String(base64String);
+
+            return ToByteArray(ToMemoryStream(bytes));
         }
 
         public static void WriteFile(MemoryStream ms, string fileName)
