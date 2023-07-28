577aa2c1c0712988e8955d3db8a9db51ce513849
yongshun950824 <yongshun950824@gmail.com>
2023-07-28 11:30:03 +0800

    EnumExtensions: - EnumMemberAttribute: Convert string to enum and vice versa

--- KYS.Library/Extensions/EnumExtensions.cs
+++ KYS.Library/Extensions/EnumExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.Serialization;
 
 namespace KYS.Library.Extensions
 {
@@ -41,6 +42,48 @@ namespace KYS.Library.Extensions
             return Enum.GetValues(typeof(TEnum))
                  .Cast<TEnum>()
                  .ToDictionary(k => k, v => v.ToDescription());
+        }
+
+        /// <summary>
+        /// Get value from <c>EnumMemberAttribute</c>.
+        /// </summary>
+        /// <typeparam name="TEnum"></typeparam>
+        /// <param name="enum"></param>
+        /// <returns></returns>
+        public static string GetEnumMemberValue<TEnum>(this TEnum @enum)
+            where TEnum : struct, Enum
+        {
+            string enumMemberValue = @enum.GetType()
+                .GetMember(@enum.ToString())
+                .FirstOrDefault()?
+                .GetCustomAttributes<EnumMemberAttribute>(false)
+                .FirstOrDefault()?
+                .Value;
+
+            if (enumMemberValue == null)
+                return @enum.ToString();
+                //throw new ArgumentException($"Enum {@enum.GetType().Name} with member {@enum} not applies the {nameof(EnumMemberAttribute)} attribute.");
+
+            return enumMemberValue;
+        }
+
+        public static TEnum EnumMemberValueToEnum<TEnum>(this string value)
+            where TEnum : struct, Enum
+        {
+            foreach (var field in typeof(TEnum).GetFields())
+            {
+                if (Attribute.GetCustomAttribute(field,
+                    typeof(EnumMemberAttribute)) is EnumMemberAttribute attribute)
+                {
+                    if (attribute.Value == value)
+                        return (TEnum)field.GetValue(null);
+                }
+
+                if (field.Name == value)
+                    return (TEnum)field.GetValue(null);
+            }
+
+            throw new ArgumentException($"{value} is not found.");
         }
 
         #region Private Methods
