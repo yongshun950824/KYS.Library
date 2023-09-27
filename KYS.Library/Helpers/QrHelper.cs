a9d9f9761a9fec8f87f2fc0d25357c30abc69bfa
yongshun950824 <yongshun950824@gmail.com>
2023-09-27 11:11:59 +0800

    Generate QR with ZXing.NET + SkiaSharp

---
+++ KYS.Library/Helpers/QrHelper.cs
@@ -0,0 +1,149 @@
+﻿using SkiaSharp;
+using System;
+using System.IO;
+using ZXing;
+using ZXing.Common;
+using ZXing.QrCode.Internal;
+
+namespace KYS.Library.Helpers
+{
+    public class QrHelper
+    {
+        private SKBitmap _bitmap;
+        private readonly string _value;
+        private readonly int _width;
+        private readonly int _height;
+        private readonly string? _logoPath;
+        private readonly int? _logoWidth;
+        private readonly int? _logoHeight;
+
+        public QrHelper(string value)
+        {
+            if (String.IsNullOrEmpty(value))
+                throw new ArgumentNullException(nameof(value));
+
+            _value = value;
+
+            if (_width == default)
+                _width = 200;
+
+            if (_height == default)
+                _height = 200;
+        }
+
+        public QrHelper(string value, int width, int height)
+            : this(value)
+        {
+            _width = width;
+            _height = height;
+        }
+
+        public QrHelper(string value, int width, int height, string logoPath)
+            : this(value, width, height)
+        {
+            if (String.IsNullOrEmpty(logoPath))
+                throw new ArgumentNullException(nameof(logoPath));
+
+            if (!File.Exists(logoPath))
+                throw new FileNotFoundException(nameof(logoPath));
+
+            _logoPath = logoPath;
+            _logoWidth = width / 5;
+            _logoHeight = height / 5;
+        }
+
+        public QrHelper(string value, int width, int height, string logoPath, int logoWidth, int logoHeight)
+            : this(value, width, height, logoPath)
+        {
+            if (width <= logoWidth)
+                throw new ArgumentException("Provided logo width must be smaller than QR code width.");
+
+            if (height <= logoHeight)
+                throw new ArgumentException("Provided logo height must be smaller than QR code height.");
+
+            if ((width * height) <= (logoWidth * logoHeight))
+                throw new ArgumentException("Provided logo area must be smaller than QR code area.");
+
+            _logoPath = logoPath;
+            _logoWidth = logoWidth;
+            _logoHeight = logoHeight;
+        }
+
+        public string Value { get { return _value; } }
+        public int Width { get { return _width; } }
+        public int Height { get { return _height; } }
+
+        public SKBitmap QRCode { get { return _bitmap; } }
+
+        public SKBitmap Draw()
+        {
+            if (_bitmap != null)
+                return _bitmap;
+
+            var encodingOptions = new EncodingOptions
+            {
+                Width = _width,
+                Height = _height,
+                Hints = { { EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H } }
+            };
+
+            var writer = new ZXing.SkiaSharp.BarcodeWriter()
+            {
+                Format = BarcodeFormat.QR_CODE,
+                Options = encodingOptions
+            };
+
+            var barcodeBitmap = writer.Write(_value);
+
+            if (!String.IsNullOrEmpty(_logoPath))
+            {
+                var logo = SKImage.FromEncodedData(_logoPath);
+                SKBitmap logoBitmap = SKBitmap.FromImage(logo);
+
+                SKBitmap overlay = logoBitmap.Resize(new SKImageInfo(_logoWidth.Value, _logoHeight.Value),
+                    SKFilterQuality.High);
+
+                int deltaHeigth = barcodeBitmap.Height - overlay.Height;
+                int deltaWidth = barcodeBitmap.Width - overlay.Width;
+
+                using var canvas = new SKCanvas(barcodeBitmap);
+                canvas.DrawBitmap(overlay, new SKPoint(deltaWidth / 2, deltaHeigth / 2));
+            }
+
+            _bitmap = barcodeBitmap;
+            return barcodeBitmap;
+        }
+
+        public MemoryStream DrawAndToMemoryStream()
+        {
+            MemoryStream ms = new MemoryStream();
+
+            SKBitmap bitmap = Draw();
+
+            bitmap.Encode(ms, SKEncodedImageFormat.Png, 100);
+            ms.Seek(0, SeekOrigin.Begin);
+
+            return ms;
+        }
+
+        public string DrawAndToDataUri()
+        {
+            using MemoryStream ms = DrawAndToMemoryStream();
+
+            return $"data:image/png;base64, {Convert.ToBase64String(ms.ToArray())}";
+        }
+
+        public void DrawAndToFile(string filePath)
+        {
+            if (!filePath.EndsWith(".png", StringComparison.OrdinalIgnoreCase))
+                filePath += ".png";
+
+            SKBitmap bitmap = Draw();
+
+            using var image = SKImage.FromBitmap(bitmap);
+            using var data = image.Encode(SKEncodedImageFormat.Png, 100);
+            using var stream = File.OpenWrite(filePath);
+            data.SaveTo(stream);
+        }
+    }
+}
