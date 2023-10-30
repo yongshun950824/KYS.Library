59e49e3ca16c39e65b9abfed6deea2a9f5ce86df
yongshun950824 <yongshun950824@gmail.com>
2023-10-30 16:16:17 +0800

    Add XmlHelper: BeautifyXml

---
+++ KYS.Library/Helpers/XmlHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Text;
+using System.Xml;
+
+namespace KYS.Library.Helpers
+{
+    public static class XmlHelper
+    {
+        public static string BeautifyXml(XmlDocument xmlDocument)
+        {
+            using StringWriter sw = new StringWriter(new StringBuilder());
+            using XmlTextWriter xmlTextWriter = new XmlTextWriter(sw) { Formatting = Formatting.Indented };
+            xmlDocument.Save(sw);
+
+            return sw.ToString();
+        }
+    }
+}
