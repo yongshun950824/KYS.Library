2b89ee6e10911c317104c3009ce450f6a76aa3cd
yongshun950824 <kokys-wa13@student.tarc.edu.my>
2025-07-12 08:51:51 +0800

    Unit of work

---
+++ KYS.EFCore.Library/Extensions/ExplictUnitOfWorkServiceCollections.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KYS.EFCore.Library.Extensions
+{
+    public static class ExplictUnitOfWorkServiceCollections
+    {
+        public static IServiceCollection AddExplicitUnitOfWork<TDbContext>(this IServiceCollection services)
+            where TDbContext : DbContext
+        {
+            services.AddScoped<IUnitOfWork>(provider =>
+            {
+                var context = provider.GetService<TDbContext>();
+                return new DbContextUnitOfWork<TDbContext>(context, true);
+            });
+
+            return services;
+        }
+    }
+}
