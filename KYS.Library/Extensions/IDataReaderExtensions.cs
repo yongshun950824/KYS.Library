b54b6fead77d92d6318da37c2e9b5b88725c8231
yongshun950824 <yongshun950824@gmail.com>
2026-02-04 07:18:03 +0000

    Add IDataReaderExtensions docs and resolve SonarQube alert

--- KYS.Library/Extensions/IDataReaderExtensions.cs
+++ KYS.Library/Extensions/IDataReaderExtensions.cs
@@ -3,8 +3,21 @@ using System.Data;
 
 namespace KYS.Library.Extensions
 {
+    /// <summary>
+    /// Provides extension methods for working with <see cref="IDataReader" />.
+    /// </summary>
     public static class IDataReaderExtensions
     {
+        /// <summary>
+        /// Safely retrieves the value from the <see cref="IDataReader" /> instance for the specified column.
+        /// <br />
+        /// Handles DBNull, type conversion, and provides specialized validation for <see cref="DateTime"/> boundaries.
+        /// </summary>
+        /// <typeparam name="T">The type of returned value and <c>defaultValue</c>.</typeparam>
+        /// <param name="reader">The <see cref="IDataReader" /> instance this method extends.</param>
+        /// <param name="columnName">The name for the data column.</param>
+        /// <param name="defaultValue">The returned value when the column is <c>DBNull</c> or contains an out-of-range DateTime.</param>
+        /// <returns>The value from the column cast to <typeparamref name="T"/>. Or <paramref name="defaultValue"/> if the column is null or an invalid date.</returns>
         public static T SafeGetValue<T>(this IDataReader reader, string columnName, T defaultValue = default)
         {
             int ordinal = reader.GetOrdinal(columnName);
